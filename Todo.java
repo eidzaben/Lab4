574a034e66389dcd5185fdd0a20ddf479e87dcd4
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-07 17:56:41 -0600

    Setup my Todo class

--- Todo.java
+++ Todo.java
@@ -1,18 +1,27 @@
 public class Todo {/*
-Lab Section_________: XX
-Last Modified_______: 20XX-XX-XX
-OU ID_______________: XXXXXXXXX
-OU EMAIL____________: XXX@ou.edu
+Lab Section_________: 11
+Last Modified_______: 2019-XX-XX
+OU ID_______________: 113334522
+OU EMAIL____________: eid.s.zaben-1@ou.edu
 
 Tasks:
-1. XXX
-2. XXX
+1. Understand pushing and pulling between git and github
+2. Create tests for Animal Test Class
+3. Create tests for EquipmentTest
+4. Create tests for ZooTest
+5. Set up a local Repo for this lab
 
 Estimated Time (minutes):
-1. XXX
-2. XXx
+1. 30
+2. 20
+3. 20
+4. 20
+5. 25
 
 Actual Time (minutes):
-1. XXX
-2. XXX
+1. 50
+2. 
+3. 
+4. 
+5. 30
 */}
