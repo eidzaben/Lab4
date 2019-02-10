6b2ebfdddd2ce85a6ac5bfbdfdae89c08f8e022f
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-10 07:09:11 -0600

    Equipment Constructor Test complete

--- src/EquipmentTest.java
+++ src/EquipmentTest.java
@@ -1,5 +1,19 @@
 public class EquipmentTest {
 	
+	public void constructorTest() throws AssertException {
+			Equipment eq = new Equipment("Batarang/5,5.0,5.0,A boomerang for bats");
+			
+			Assert.assertEquals("Batarang",  eq.getName());
+			Assert.assertEquals(5, eq.getCount());
+			Assert.assertEquals(5.0, eq.getTotalWeight(), .01);
+			Assert.assertEquals(5.0, eq.getTotalPrice(), .01);
+			Assert.assertEquals("A boomerang for bats", eq.getDescription());
+			
+			
+}
+	
+	
+	
 
 
 }
