a8ed051b188fcfd7597fdf93b1027c38a29a58af
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-02-10 07:31:30 -0600

    ZooTest ConstructorTest complete

--- src/ZooTest.java
+++ src/ZooTest.java
@@ -1,5 +1,16 @@
-public class ZooTest {
-	
+	public class ZooTest {
+		
+		public void zooConstructorTest() throws AssertException {
+				Zoo zoo = new Zoo(20);
+			//	int expectedAnimals = 20;
+				int expCapacity = 20;
+				Animal[] expAnimals = new Animal[20];
+				
+				Assert.assertEquals(expCapacity, zoo.getCapacity());
+				Assert.assertEquals(expAnimals, zoo.getAnimals());
+			//	Assert.assertEquals(
+				
+			}
 	
 	
 }
